e8161d49039019c9ab794a09eb8c4ca24898b760
Helmi K <72031938+karhelmi@users.noreply.github.com>
2021-05-04 15:16:01 +0300

    Delete build_database_for_tests.py
    Useless file deleted.

--- src/build_database_for_tests.py
+++
@@ -1,9 +0,0 @@
-#from database_initialize import initialize_database
-
-
-# def build_database_for_tests():
-#   initialize_database()
-
-
-# f __name__ == '__main__':
-#   build_database_for_tests()
